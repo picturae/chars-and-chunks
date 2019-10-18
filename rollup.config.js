900d2b76e7d34994d88c1c5ecea523f6d6abfb0f
ovanderzee <hastalavista@dds.nl>
2019-10-18 13:31:12 +0200

    package setup

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+// rollup.config.js
+import commonjs from 'rollup-plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+
+const name = 'charsAndChuncks'
+
+export default {
+  input: `src/index.js`,
+  output: [
+    {
+      file: `dist/${name}.js`,
+      format: 'umd',
+      name: name,
+      sourcemap: true,
+    },
+    {
+      file: `module/${name}.js`,
+      format: 'esm',
+      name: name,
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    commonjs({
+      // to read umd dependencies
+      include: 'node_modules/**',
+    }),
+    terser(),
+  ],
+}
