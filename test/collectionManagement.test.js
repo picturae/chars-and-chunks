a5b5a6a97fb0a31fe6d2fa222953a869c70b2bae
ovanderzee <hastalavista@dds.nl>
2019-10-28 10:15:54 +0100

    test for reset and regex cascade

--- test/collectionManagement.test.js
+++ test/collectionManagement.test.js
@@ -95,4 +95,48 @@ describe('Good registration is handled well', function() {
 
     expect(spyCallback).toHaveBeenCalledWith(entry)
   })
+
+  test('All registrations can be reset', () => {
+    collectionManagement.registerHotkey(registrations.OK)
+    collectionManagement.registerBarcode(registrations.OK2)
+    const handle = collectionManagement.hotkeyHandler(registrations.OK.char)
+    const handle2 = collectionManagement.barcodeHandler(registrations.OK2.regex)
+
+    collectionManagement.reset()
+
+    const handle_reset = collectionManagement.hotkeyHandler(
+      registrations.OK.char,
+    )
+    const handle2_reset = collectionManagement.barcodeHandler(
+      registrations.OK2.regex,
+    )
+
+    expect(handle).toBeTruthy
+    expect(handle2).toBeTruthy
+    expect(handle_reset).toBeFalsy
+    expect(handle2_reset).toBeFalsy
+  })
+
+  test(`A barcode is paired to the lengthiest matching Regexp`, () => {
+    const entryOmit = '32457+5456'
+    const spyCallbackOmit = jest.spyOn(registrations.omittedRegex, 'callback')
+    collectionManagement.registerBarcode(registrations.omittedRegex)
+    collectionManagement.barcodeHandler(entryOmit).callback(entryOmit)
+
+    expect(spyCallbackOmit).toHaveBeenCalledWith(entryOmit)
+
+    const entrySimple = 'HEERHUGOWAARD'
+    const spyCallbackSimple = jest.spyOn(registrations.simpleRegex, 'callback')
+    collectionManagement.registerBarcode(registrations.simpleRegex)
+    collectionManagement.barcodeHandler(entrySimple).callback(entrySimple)
+
+    expect(spyCallbackSimple).toHaveBeenCalledWith(entrySimple)
+
+    const entryAdv = 'RESULT_NOT_OK'
+    const spyCallbackAdv = jest.spyOn(registrations.advancedRegex, 'callback')
+    collectionManagement.registerBarcode(registrations.advancedRegex)
+    collectionManagement.barcodeHandler(entryAdv).callback(entryAdv)
+
+    expect(spyCallbackAdv).toHaveBeenCalledWith(entryAdv)
+  })
 })
